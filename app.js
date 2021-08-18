a799d908df9f847f20a1a8cbd38a01f21e643c10
DanielRGilles <danryangilles@gmail.com>
2021-08-18 15:45:36 -0700

    built whoWon function

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
-import { computersThrow } from './get-random-throw.js';
+import { computersThrow, whoWon } from './get-random-throw.js';
 
 // import functions and grab DOM elements
+
+let wins = 0;
+let loss = 0;
+let draws = 0;
 
 const buttonEl = document.querySelector('#submit');
 const winsEl = document.querySelector('#loss-total');
 const lossEl = document.querySelector('#wins-total');
 const resultEl = document.querySelector('#throw-result');
 // initialize global state
-let wins = 0;
-let loss = 0;
-let daws = 0;
+
 let compThrow = null;
 
 // set event listeners 
-function whoWon(userThrow, compThrow) {
-    if (userThrow === compThrow)
-        return resultEl.textContent = 'Draw';
-}
+
 buttonEl.addEventListener('click', () => {
     const userSelectedThrow = document.querySelector('input[type=radio]:checked');
-
     let userThrow = userSelectedThrow.value;
- console.log(userThrow);
+//  console.log(userThrow);
  // get user input
     compThrow = computersThrow();
-console.log(compThrow);  
-    whoWon(userThrow, compThrow);
+// console.log(compThrow);  
+    whoWon(userThrow, compThrow) {
+      if 
+    }
+
+    
    
-console.log(whoWon());
 });
- 
+
+
   // use user input to update state 
   // update DOM to reflect the new state
+  
+      
--- get-random-throw.js
+++ get-random-throw.js
@@ -9,3 +9,32 @@ export function computersThrow() {
     }
 
 }
+
+
+
+export function whoWon(userThrow, compThrow) {
+    if (userThrow.value === compThrow.value) {
+        return 'draw';
+    } else if (userThrow.value === 'Paper' && compThrow.value === 'Rock') {
+        return 'win';
+    }
+    else if (userThrow.value === 'Paper' && compThrow.value === 'Scissors') {
+        return 'lose';
+
+    }
+    else if (userThrow.value === 'Rock' && compThrow.value === 'Paper') {
+        return 'lose';
+    } 
+    else if (userThrow.value === 'Rock' && compThrow.value === 'Scissors') {
+        return 'win';
+
+    }
+    else if (userThrow.value === 'Scissors' && compThrow.value === 'Rock') {
+        return 'lose';
+
+    }
+    else if (userThrow.value === 'Scissors' && compThrow.value === 'Paper') {
+        return 'win';
+    }
+    
+}
