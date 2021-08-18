e14fa11d94de1d5672d2989edc88d696b60578b1
DanielRGilles <danryangilles@gmail.com>
2021-08-18 14:37:56 -0700

    tested throw metric

--- app.js
+++ app.js
@@ -5,21 +5,29 @@ import { computersThrow } from './get-random-throw.js';
 const buttonEl = document.querySelector('#submit');
 const winsEl = document.querySelector('#loss-total');
 const lossEl = document.querySelector('#wins-total');
+const resultEl = document.querySelector('#throw-result');
 // initialize global state
 let wins = 0;
 let loss = 0;
+let daws = 0;
 let compThrow = null;
-// set event listeners 
 
+// set event listeners 
+function whoWon(userThrow, compThrow) {
+    if (userThrow === compThrow)
+        return resultEl.textContent = 'Draw';
+}
 buttonEl.addEventListener('click', () => {
     const userSelectedThrow = document.querySelector('input[type=radio]:checked');
 
     let userThrow = userSelectedThrow.value;
-//  console.log(userThrow);
+ console.log(userThrow);
  // get user input
     compThrow = computersThrow();
 console.log(compThrow);  
-
+    whoWon(userThrow, compThrow);
+   
+console.log(whoWon());
 });
  
   // use user input to update state 
--- test/example.test.js
+++ test/example.test.js
@@ -1,17 +1,19 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { computersThrow } from '../get-random-throw.js';
+
+
 
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
+    const options = ['Rock', 'Paper', 'Scissors'];
     const expected = true;
-    
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
-
+    
+    const actual = options.includes(computersThrow());
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
